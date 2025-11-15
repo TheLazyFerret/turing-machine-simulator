46418e15263c341a0f4977654ed0d45b8df9af3f
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2025-11-15 00:08:15 +0000

    write and read methods done

--- src/tape/mod.rs
+++ src/tape/mod.rs
@@ -6,6 +6,7 @@
 //! Module representing a turing machine.
 
 use core::fmt;
+use std::ops::{Neg, Sub};
 
 /// Represents a blank in a tape's cell.
 const BLANK: char = '\0';
@@ -31,21 +32,59 @@ impl Tape {
   /// Moves the head one cell to left. If the cell is not defined, create a blank one.
   pub fn move_left(&mut self) {}
 
+  /// Returns the value in the head position.
+  pub fn read(&self) -> char {
+    if self.head.is_positive() {
+      *self.p_half.get(self.normalized_head()).expect("weird error accesing for read")
+    }
+    else {
+      *self.n_half.get(self.normalized_head()).expect("weird error accesing for read")
+    }
+  }
+
+  /// Write a char in the head position.
+  pub fn write(&mut self, f: char) {
+    let pos = self.normalized_head();
+    if self.head.is_positive() {
+      *self.p_half.get_mut(pos).expect("weird error accesing for write") = f;
+    }
+    else {
+      *self.p_half.get_mut(pos).expect("weird error accesing for write") = f;
+    }
+  }
+
   /// Loads a string into the Tape.
   pub fn load_string(&mut self, f: &str) {
     self.clean();
     self.p_half = f.chars().collect();
   }
 
+}
+
+/// Private implementation for Tape struct.
+impl Tape {
   /// Clean the current state of the object.
   fn clean(&mut self) {
     self.n_half.clear();
     self.p_half.clear();
     self.head = 0;
   }
+
+  /// Normalize the value of the head to peak in one of the vectors.
+  ///  - head >= 0 returns head
+  ///  - head < 0 returns head - 1
+  fn normalized_head(&self) -> usize {
+    if self.head.is_positive() {
+      self.head.cast_unsigned()
+    }
+    else {
+      self.head.neg().cast_unsigned().sub(1)
+    }
+  }
 }
 
 impl Default for Tape {
+  /// Default construction for Tape.
   fn default() -> Self {
     Tape {
       n_half: Vec::new(),
