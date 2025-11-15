b85790355066213d93f6778fcc9aa505a445a275
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2025-11-15 09:46:26 +0000

    move functions, from_string, and fmt::Display for Tape

--- src/tape/mod.rs
+++ src/tape/mod.rs
@@ -6,10 +6,13 @@
 //! Module representing a turing machine.
 
 use core::fmt;
-use std::ops::{Add, Neg, Sub};
+use std::ops::{Neg, Sub};
 
 /// Represents a blank in a tape's cell.
 const BLANK: char = '\0';
+
+/// How the blanks will be printed.
+const BLANK_REP: char = 'β';
 
 /// Struct representing a Single tape.
 #[derive(Clone, Debug)]
@@ -26,11 +29,34 @@ impl Tape {
     Tape::default()
   }
 
+  /// Returns a new Tape with a string loaded.
+  pub fn from_string(f: &str) -> Self {
+    Tape {
+      n_half: Vec::new(),
+      p_half: f.chars().collect(),
+      head: 0,
+    }
+  }
+
   /// Moves the head one cell to right. If the cell is not defined, create a blank one.
-  pub fn move_right(&mut self) {}
+  /// It is only defined to create a new cell if the head is in the positive side.
+  pub fn move_right(&mut self) {
+    self.head += 1;
+    let norm = self.normalized_head();
+    if self.head >= 0 && self.p_half.get(norm).is_none() {
+      self.p_half.push(BLANK);
+    }
+  }
 
   /// Moves the head one cell to left. If the cell is not defined, create a blank one.
-  pub fn move_left(&mut self) {}
+  /// It is only defined to create a new cell if the head is in the negative half.
+  pub fn move_left(&mut self) {
+    self.head -= 1;
+    let norm = self.normalized_head();
+    if self.head < 0 && self.n_half.get(norm).is_none() {
+      self.n_half.push(BLANK);
+    }
+  }
 
   /// Returns the value in the head position.
   pub fn read(&self) -> char {
@@ -102,29 +128,49 @@ impl Default for Tape {
   }
 }
 
+/// Auxiliar function for Display: returns a visual representation of a symbol.
+/// Basically prints β if the symbol is a blank, itself otherwise.
+fn print_sym(x: char) -> char {
+  if x == BLANK { BLANK_REP } else { x }
+}
+
 impl fmt::Display for Tape {
-  #[allow(unused_variables)]
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    todo!()
+    for x in self.n_half.iter().enumerate() {
+      let relative_pos = (self.n_half.len().cast_signed() - x.0.cast_signed()).neg();
+      if relative_pos == self.head {
+        write!(f, "|[{}]", print_sym(*x.1))?;
+      } else {
+        write!(f, "|{}", print_sym(*x.1))?;
+      }
+    }
+    for x in self.p_half.iter().enumerate() {
+      let relative_pos = x.0.cast_signed();
+      if relative_pos == self.head {
+        write!(f, "|[{}]", print_sym(*x.1))?;
+      } else {
+        write!(f, "|{}", print_sym(*x.1))?;
+      }
+    }
+    write!(f, "|")?;
+    Ok(())
   }
 }
 
 #[cfg(test)]
 mod tests {
-  use crate::tape::Tape;
+  use crate::tape::{BLANK, Tape};
 
   #[test]
-  fn test_load_string() {
-    let mut x = Tape::new();
-    x.load_string("perro");
+  fn test_from_string() {
+    let x = Tape::from_string("perro");
     assert_eq!(*x.p_half.as_slice().first().unwrap(), 'p');
     assert_eq!(*x.p_half.as_slice().last().unwrap(), 'o');
   }
 
   #[test]
   fn test_write_read_head() {
-    let mut x = Tape::new();
-    x.load_string("perro");
+    let mut x = Tape::from_string("perro");
     x.head = 1;
     x.write('@');
     assert_eq!(x.read(), '@');
@@ -139,5 +185,28 @@ mod tests {
     assert_eq!(x.normalized_head(), 0);
     x.head = -2;
     assert_eq!(x.normalized_head(), 1);
+  }
+
+  #[test]
+  fn test_move() {
+    let mut x = Tape::from_string("p");
+    x.move_left();
+    assert_eq!(x.read(), BLANK);
+    x.move_right();
+    assert_eq!(x.read(), 'p');
+    x.move_right();
+    assert_eq!(x.read(), BLANK);
+  }
+
+  #[test]
+  fn test_display() {
+    let mut x = Tape::from_string("p");
+    x.move_left();
+    x.move_right();
+    x.move_right();
+    x.move_right();
+    x.move_left();
+    let rep = x.to_string();
+    assert_eq!(rep, "|β|p|[β]|β|");
   }
 }
